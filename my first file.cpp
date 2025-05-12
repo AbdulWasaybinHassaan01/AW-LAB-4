d5f65b57952d140368930775c5746875a015c5f7
AbdulWasaybinHassaan <universityworkonly01@gmail.com>
2025-05-12 23:34:22 +0500

    Update my first file.cpp

--- my first file.cpp
+++ my first file.cpp
@@ -9,10 +9,20 @@ cout << "Sum is: " << sum;
 
 
 int x, y;
-int sum;
+int subtract;
 cout << "Type a number: ";
 cin >> x;
 cout << "Type another number: ";
 cin >> y;
 subtract = x - y;
 cout << "Subract is: " << sum;
+
+
+int x, y;
+int product;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+product = x * y;
+cout << "Product is: " << sum;
